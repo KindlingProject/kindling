59fe8d3fc956244dfa0d113caab17ae36c8a88e5
sanyangji <songyujie@zju.edu.cn>
2022-01-25 15:49:24 +0800

    Update: deploy files
    Signed-off-by: sanyangji <songyujie@zju.edu.cn>

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+kubectl create ns kindling
 
 kubectl create serviceaccount kindling-agent -nkindling
 kubectl apply -f kindling-clusterrole.yml
 kubectl create clusterrolebinding kindling-agent --clusterrole=kindling-agent --serviceaccount=kindling:kindling-agent
-kubectl apply -f kindling-configmap.yml
+kubectl create cm kindlingcfg -n kindling --from-file=kindling-collector-config.yml
 kubectl apply -f kindling-deploy.yml
+
+# configure Prometheus
+kubectl apply -f kindling-prometheus-rabc.yml
+kubectl apply -f kindling-prometheus-service.yml
+kubectl apply -f kindling-prometheus-servicemonitor.yml
 
 kubectl get pods -n kindling
--- deploy/uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl delete ds kindling-agent -n kindling
-kubectl delete cm kindlingcfg -n kindling
-kubectl delete -f kindling-clusterrole.yml
-kubectl delete serviceaccounts kindling-agent -n kindling
-kubectl delete clusterrolebindings kindling-agent
