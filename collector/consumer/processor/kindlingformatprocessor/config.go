b678d19c29690b6a2ea0e93e6e2eb0c7ecc66960
NeJan2020 <64676199+NeJan2020@users.noreply.github.com>
2022-03-09 18:44:16 +0800

    feat: add support for sampling at format-processor (#94)
    * feat: add support for sampling at format-processor
    
    Signed-off-by: niejiangang <niejiangang@harmonycloud.cn>
    
    * style: use a better if exp
    
    Signed-off-by: niejiangang <niejiangang@harmonycloud.cn>
    
    * style: use a better var name
    
    Signed-off-by: niejiangang <niejiangang@harmonycloud.cn>
    
    * fix: default value
    
    Signed-off-by: niejiangang <niejiangang@harmonycloud.cn>
    
    * fix: sample condition
    
    Signed-off-by: niejiangang <niejiangang@harmonycloud.cn>
    
    * fix: sample condition
    
    Signed-off-by: niejiangang <niejiangang@harmonycloud.cn>
    
    * style: code style change
    
    Signed-off-by: niejiangang <niejiangang@harmonycloud.cn>

--- collector/consumer/processor/kindlingformatprocessor/config.go
+++ collector/consumer/processor/kindlingformatprocessor/config.go
@@ -1,8 +1,15 @@
 package kindlingformatprocessor
 
 type Config struct {
-	NeedTraceAsResourceSpan bool `mapstructure:"need_trace_as_span"`
-	NeedTraceAsMetric       bool `mapstructure:"need_trace_as_metric"`
-	NeedPodDetail           bool `mapstructure:"need_pod_detail"`
-	StoreExternalSrcIP      bool `mapstructure:"store_external_src_ip"`
+	NeedTraceAsResourceSpan bool          `mapstructure:"need_trace_as_span"`
+	NeedTraceAsMetric       bool          `mapstructure:"need_trace_as_metric"`
+	NeedPodDetail           bool          `mapstructure:"need_pod_detail"`
+	StoreExternalSrcIP      bool          `mapstructure:"store_external_src_ip"`
+	SamplingRate            *SampleConfig `mapstructure:"sampling_rate"`
+}
+
+type SampleConfig struct {
+	NormalData int `mapstructure:"normal_data"`
+	SlowData   int `mapstructure:"slow_data"`
+	ErrorData  int `mapstructure:"error_data"`
 }
