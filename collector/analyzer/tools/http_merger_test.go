a5b1834cb1a999b5e36b8108f4310ccc93f33d8d
zheng <huxiangyuan@harmonycloud.cn>
2022-03-16 18:12:20 +0800

    FIX concurrent map read and map write with lrucache (#117)
    * FIX concurrent map read and map write with lrucache
    
    Signed-off-by: zheng <huxiangyuan@harmonycloud.cn>
    
    * Add Concurrent Test
    
    Signed-off-by: zheng <huxiangyuan@harmonycloud.cn>
    
    * Concurrent map is not panic, cannot be recovered
    
    Signed-off-by: zheng <huxiangyuan@harmonycloud.cn>

--- collector/analyzer/tools/http_merger_test.go
+++ collector/analyzer/tools/http_merger_test.go
@@ -1,7 +1,11 @@
 package tools
 
 import (
+	"strconv"
+	"sync"
 	"testing"
+
+	"github.com/hashicorp/golang-lru/simplelru"
 )
 
 func TestHttpMerger_ConvergeByAlgorithm(t *testing.T) {
@@ -92,4 +96,72 @@ func TestHttpMerger_TruncateStarUrl(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestHttpMerger_Cache(t *testing.T) {
+	httpMerger := &HttpMerger{}
+	httpMerger.cache, _ = simplelru.NewLRU(3, nil)
+
+	tests := []struct {
+		testName string
+		size     int
+		urlA     string
+		urlB     string
+		wantA    int
+		wantB    int
+	}{
+		{"Empty Cache", 0, "a", "b", -1, -1},      // Empty
+		{"Match One Key", 2, "a", "c", 1, -1},     // a, b
+		{"Match Another Key", 3, "a", "d", 2, -1}, // b, a, c
+		{"Overflow", 3, "c", "d", 1, 1},           // c, a, d
+		{"LRU Cache", 3, "a", "b", 3, -1},         // a, c, d
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.testName, func(t *testing.T) {
+			cacheSize := httpMerger.cache.Len()
+			if cacheSize != tt.size {
+				t.Errorf("Expect cache Size got %v, want %v", cacheSize, tt.size)
+			}
+			valueA, valueB := httpMerger.getCountByUrl(tt.urlA, tt.urlB)
+			if valueA != tt.wantA {
+				t.Errorf("Expect valueA got %v, want %v", valueA, tt.wantA)
+			}
+			if valueB != tt.wantB {
+				t.Errorf("Expect valueB got %v, want %v", valueB, tt.wantB)
+			}
+
+			if valueA < 0 && valueB > 0 {
+				httpMerger.addUrlCount(tt.urlA, 1, tt.urlB, valueB+1)
+			} else if valueA > 0 && valueB < 0 {
+				httpMerger.addUrlCount(tt.urlA, valueA+1, tt.urlB, 1)
+			} else if valueA < 0 && valueB < 0 {
+				httpMerger.addUrlCount(tt.urlA, 1, tt.urlB, 1)
+			}
+		})
+	}
+}
+
+func TestConcurrentLruCache(t *testing.T) {
+	httpMerger := &HttpMerger{}
+	httpMerger.cache, _ = simplelru.NewLRU(10, nil)
+	var wg sync.WaitGroup
+
+	addFunc := func() {
+		urlA, urlB := "aaa", "bbb"
+		for i := 0; i < 100; i++ {
+			suffix := strconv.Itoa(i)
+			httpMerger.getCountByUrl(urlA+suffix, urlB+suffix)
+			httpMerger.addUrlCount(urlA+suffix, 1, urlB+suffix, 1)
+		}
+
+		wg.Done()
+	}
+
+	for threads := 0; threads < 4; threads++ {
+		wg.Add(1)
+		go addFunc()
+	}
+
+	wg.Wait()
 }
