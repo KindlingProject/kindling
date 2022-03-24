471ad4d045afdca86d1bb09dd525a8b42166fc24
sugary199 <906940958@qq.com>
2022-03-24 15:07:46 +0800

    add core,add version to collecotr/main.go print commitId once before starting application
    Signed-off-by: sugary199 <906940958@qq.com>

--- collector/main.go
+++ collector/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"github.com/Kindling-project/kindling/collector/application"
 	"log"
+	"fmt"
+
+	"github.com/sugary199/collector-version/core"
 )
 
 func main() {
@@ -10,8 +13,19 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to create application: %v", err)
 	}
+	
+	//print version information
+	fmt.Printf("%s\n", core.Version())
+	log.Printf("GitCommitInfo:%s\n", core.Version())
+	log.Printf("-------------")
+	log.Printf("-------------")
+	log.Printf("-------------")
+	log.Printf("-------------")
+
+	
 	err = app.Run()
 	if err != nil {
 		log.Fatalf("Failed to run application: %v", err)
 	}
+
 }
