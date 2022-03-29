cf0232023789964379811e5af28689cc892cae9d
sugary199 <906940958@qq.com>
2022-03-29 14:31:43 +0800

    Print the version of collector when starting (#152)
    * add core,add version to collecotr/main.go
    print commitId once before starting application
    
    Signed-off-by: sugary199 <906940958@qq.com>
    
    * print version for collector
    
    Signed-off-by: sugary199 <906940958@qq.com>
    
    * change core to version
    
    Signed-off-by: sugary199 <906940958@qq.com>
    
    * Add the command of using formal repository after merging
    
    Signed-off-by: sugary199 <906940958@qq.com>
    
    * Add the command and package name of using formal repository after merging
    
    Signed-off-by: sugary199 <906940958@qq.com>
    
    * Deleted personal related section
    
    Signed-off-by: sugary199 <906940958@qq.com>
    
    * Changed the mount point of container
    Separated startup and compilation
    Standardized the format of main.go
    
    Signed-off-by: sugary199 <906940958@qq.com>
    
    * Delete irrelevant content in collector-version-buiild
    
    Signed-off-by: sugary199 <906940958@qq.com>

--- collector/main.go
+++ collector/main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"github.com/Kindling-project/kindling/collector/application"
+	"github.com/Kindling-project/kindling/collector/version"
 	"log"
 )
 
 func main() {
+	//print version information
+	log.Printf("GitCommitInfo:%s\n", version.Version())
+
 	app, err := application.New()
 	if err != nil {
 		log.Fatalf("Failed to create application: %v", err)
 	}
+
 	err = app.Run()
 	if err != nil {
 		log.Fatalf("Failed to run application: %v", err)
 	}
+
 }
