f8b84b67c350d08f52a93aa901a003c0919a246c
zheng <huxiangyuan@harmonycloud.cn>
2022-04-01 17:03:33 +0800

    Fix MultiTopics in kafka payload will be discerned as NOSUPPORT (#165)
    * Fix MultiTopics in kafka payload will be discerned as NOSUPPORT
    
    Signed-off-by: zheng <huxiangyuan@harmonycloud.cn>
    
    * Add reason for get firsttopic
    
    Signed-off-by: zheng <huxiangyuan@harmonycloud.cn>
    
    * Add reason in comment
    
    Signed-off-by: zheng <huxiangyuan@harmonycloud.cn>

--- collector/analyzer/network/protocol/kafka/kafka_request_fetch.go
+++ collector/analyzer/network/protocol/kafka/kafka_request_fetch.go
@@ -14,12 +14,10 @@ func fastfailRequestFetch() protocol.FastFailFn {
 func parseRequestFetch() protocol.ParsePkgFn {
 	return func(message *protocol.PayloadMessage) (bool, bool) {
 		var (
-			offset       int
-			err          error
-			topicNum     int32
-			topicName    string
-			partitionNum int32
-			partition    int32
+			offset    int
+			err       error
+			topicNum  int32
+			topicName string
 		)
 		version := message.GetIntAttribute(constlabels.KafkaVersion)
 		compact := version >= 12
@@ -36,20 +34,24 @@ func parseRequestFetch() protocol.ParsePkgFn {
 			offset += 8 // session_id, session_epoch
 		}
 
-		if offset, err = message.ReadArraySize(offset, compact, &topicNum); err != nil || topicNum != 1 {
+		if offset, err = message.ReadArraySize(offset, compact, &topicNum); err != nil {
 			return false, true
 		}
-		if offset, err = message.ReadString(offset, compact, &topicName); err != nil {
-			return false, true
+		if topicNum > 0 {
+			if _, err = message.ReadString(offset, compact, &topicName); err != nil {
+				return false, true
+			}
+			/*
+				Based on following case, we	just read first topicName.
+
+				1. The payload is substr with fixed length(1K), it's not able to get all topic names
+				2. Even if we get enough length for payload, the parser will take more performance cost
+				3. There is not enough cases to cover multi-topics
+
+				Since version 13, topicName will be repalced with topicId as uuid, therefore topicName is not able to be got.
+			*/
+			message.AddUtf8StringAttribute(constlabels.KafkaTopic, topicName)
 		}
-		if offset, err = message.ReadArraySize(offset, compact, &partitionNum); err != nil || partitionNum != 1 {
-			return false, true
-		}
-		if _, err = message.ReadInt32(offset, &partition); err != nil {
-			return false, true
-		}
-		message.AddUtf8StringAttribute(constlabels.KafkaTopic, topicName)
-		message.AddIntAttribute(constlabels.KafkaPartition, int64(partition))
 
 		return true, true
 	}
