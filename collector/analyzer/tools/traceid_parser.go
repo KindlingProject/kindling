48381d3a1e452b51b5302a1fd4083bfa387c6587
zheng <huxiangyuan@harmonycloud.cn>
2022-04-26 17:37:44 +0800

    FIX incorrect traceType for HTTP header parser (#199)
    Signed-off-by: huxiangyuan <huxiangyuan@harmonycloud.cn>

--- collector/analyzer/tools/traceid_parser.go
+++ collector/analyzer/tools/traceid_parser.go
@@ -3,23 +3,22 @@ package tools
 import "strings"
 
 func ParseTraceHeader(headers map[string]string) (traceType string, traceId string) {
-	if eagleEye, ok := headers["eagleeye-traceid"]; ok {
-		return "arms", eagleEye
-	}
-
 	if zipkin, ok := headers["x-b3-traceid"]; ok {
 		return "zipkin", zipkin
 	}
 
 	if jaeger, ok := headers["uber-trace-id"]; ok {
 		if pos := strings.Index(jaeger, ":"); pos > 0 {
-			return "zipkin", jaeger[0:pos]
+			return "jaeger", jaeger[0:pos]
 		}
-		return "zipkin", jaeger
+		return "jaeger", jaeger
 	}
 
-	if w3c, ok := headers["traceparent"]; ok && len(w3c) >= 32 {
-		return "w3c", w3c[3:32]
+	if w3c, ok := headers["traceparent"]; ok && len(w3c) >= 35 {
+		return "w3c", w3c[3:35]
+	}
+	if w3c, ok := headers["traceresponse"]; ok && len(w3c) >= 35 {
+		return "w3c", w3c[3:35]
 	}
 
 	return "", ""
