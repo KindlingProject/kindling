892c12ae44b1b3bcb02bdc0f819dc51ad4d4a7ae
sangyangji <songyujie@zju.edu.cn>
2022-06-20 14:23:29 +0800

    fix libperf
    Signed-off-by: sangyangji <songyujie@zju.edu.cn>

--- probe/cmake/modules/perf.cmake
+++ probe/cmake/modules/perf.cmake
@@ -8,12 +8,12 @@ set(PERF_INCLUDE_DIR "${PERF_SRC}/tools/lib/perf/include")
 ExternalProject_Add(perf
         PREFIX "${PROJECT_BINARY_DIR}/perf-prefix"
         URL "https://github.com/hocktea214/libperf/archive/refs/tags/v1.0.0.tar.gz"
-        URL_HASH "SHA256=ed38b724e8986b114f0b3214d552c9e2608ff262eedfe6858a98aebec952abae"
+        URL_HASH "SHA256=26c0c75194c87fe02b7bec7a64bd4c31ee1ee98e58a753e0cda4d6994591d05c"
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ${CMD_MAKE}
         BUILD_BYPRODUCTS ${PERF_LIB}
         BUILD_IN_SOURCE 1
         INSTALL_COMMAND "")
 include_directories(${PERF_INCLUDE_DIR})
-#include_directories("${PERF_SRC}/tools/include/uapi")
-#include_directories("${PERF_SRC}/tools/include")
+execute_process(COMMAND mkdir "${PERF_INCLUDE_DIR}/linux")
+execute_process(COMMAND cp "${PERF_SRC}/tools/include/uapi/linux/perf_event.h" "${PERF_INCLUDE_DIR}/linux")
