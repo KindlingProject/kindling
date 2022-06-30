d5e0d8c8fc296019267c5ad8b7025ad64536ac8f
Daxin Wang <daxinwang@harmonycloud.cn>
2022-06-30 11:26:18 +0800

    update versions
    Signed-off-by: Daxin Wang <daxinwang@harmonycloud.cn>

--- deploy/recompile-probe/recompile-module.sh
+++ deploy/recompile-probe/recompile-module.sh
@@ -5,11 +5,11 @@ cd rebuild-kindling-agent
 curl -O https://k8s-bpf-probes-public.oss-cn-hangzhou.aliyuncs.com/agent-libs-${AGENT_LIBS_COMMIT_ID}.tar.gz
 tar -zvxf agent-libs-${AGENT_LIBS_COMMIT_ID}.tar.gz
 cd agent-libs-${AGENT_LIBS_COMMIT_ID}
-docker pull kindlingproject/kernel-builder:latest
+docker pull kindlingproject/kernel-builder:v0.3.0
 docker run -it -v /usr:/host/usr -v /lib/modules:/host/lib/modules -v $PWD:/source kindlingproject/kernel-builder:latest
 cd ..
 cat <<EOF > Dockerfile
 FROM kindlingproject/kindling-agent:latest
 COPY ./agent-libs-${AGENT_LIBS_COMMIT_ID}/kindling-falcolib-probe/* /opt/kindling-extra-probe/
 EOF
-docker build -t kindlingproject/kindling-probe:latest-bymyself .
+docker build -t kindlingproject/kindling-agent:v0.3.0-bymyself .
