3c36663e1a4d1d6f97a0422479553cb18fecb4b5
Daxin Wang <daxinwang@harmonycloud.cn>
2022-08-03 11:04:18 +0800

    fix the bug where no HTTP headers were got
    Signed-off-by: Daxin Wang <daxinwang@harmonycloud.cn>

--- collector/pkg/component/analyzer/network/protocol/http/http_parser_test.go
+++ collector/pkg/component/analyzer/network/protocol/http/http_parser_test.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/Kindling-project/kindling/collector/pkg/component/analyzer/network/protocol"
@@ -134,6 +135,66 @@ func TestParseHttpResponse_GetPayLoad(t *testing.T) {
 			}
 			if got := message.GetStringAttribute(constlabels.HttpResponsePayload); got != tt.want {
 				t.Errorf("GetHttpPayload() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parseHeaders(t *testing.T) {
+	type args struct {
+		message *protocol.PayloadMessage
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string]string
+	}{
+		{
+			name: "normal case",
+			args: args{
+				message: protocol.NewRequestMessage([]byte("HTTP/1.1 200 OK\r\nConnection: keep-alive\r\nAPM-AgentID: TTXvC3EQS6KLwxx3eIqINFjAW2olRm+cr8M+yuvwhkY=\r\nTransfer-Encoding: chunked\r\nContent-Type: application/json\r\nAPM-TransactionID: 5e480579c718a4a6498a9")),
+			},
+			want: map[string]string{
+				"connection":        "keep-alive",
+				"apm-agentid":       "TTXvC3EQS6KLwxx3eIqINFjAW2olRm+cr8M+yuvwhkY=",
+				"transfer-encoding": "chunked",
+				"content-type":      "application/json",
+				"apm-transactionid": "5e480579c718a4a6498a9",
+			},
+		},
+		{
+			name: "no values",
+			args: args{
+				protocol.NewRequestMessage([]byte("HTTP/1.1 200 OK\r\nConnection: keep-alive\r\nTransfer-Encoding: ")),
+			},
+			want: map[string]string{
+				"connection": "keep-alive",
+			},
+		},
+		{
+
+			name: "no spaces",
+			args: args{
+				protocol.NewRequestMessage([]byte("HTTP/1.1 200 OK\r\nConnection: keep-alive\r\nTransfer-Encoding:")),
+			},
+			want: map[string]string{
+				"connection": "keep-alive",
+			},
+		},
+		{
+			name: "no colon",
+			args: args{
+				protocol.NewRequestMessage([]byte("HTTP/1.1 200 OK\r\nConnection: keep-alive\r\nTransfer-Encoding")),
+			},
+			want: map[string]string{
+				"connection": "keep-alive",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parseHeaders(tt.args.message); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseHeaders() = %v, want %v", got, tt.want)
 			}
 		})
 	}
