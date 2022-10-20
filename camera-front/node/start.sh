7b7644b0e5eeac73d7e6437e3f79026c6b8c56d8
Daxin Wang <idxwang@outlook.com>
2022-10-20 19:19:32 +0800

    Add Dockerfile of camera-front
    Signed-off-by: Daxin Wang <idxwang@outlook.com>

--- camera-front/node/start.sh
+++ camera-front/node/start.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
  cd `dirname $0`
  pids=$(netstat -ltnp|grep 9900|awk '{print $7}');
  if [ $pids ] ; then
-  pm2 restart camera-node         
+  pm2-runtime restart camera-node         
  else
-  pm2 start index.js --name camera-node 
+  pm2-runtime start index.js --name camera-node 
  fi 
 #  pids=$(netstat -ltnp|grep 9091|awk '{print $7}');
 #  if [ $pids ] ; then
