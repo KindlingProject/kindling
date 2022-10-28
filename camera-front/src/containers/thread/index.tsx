f1c48bb3a74552fe00d6c4986ebb4f7c74100f29
yiqianxu <yiqianxu@harmonycloud.cn>
2022-10-28 15:02:22 +0800

    theme params fix
    Signed-off-by: yiqianxu <yiqianxu@harmonycloud.cn>

--- camera-front/src/containers/thread/index.tsx
+++ camera-front/src/containers/thread/index.tsx
@@ -254,7 +254,7 @@ function Thread() {
         let theme: any = searchParams.get('theme') || 'light';
         let body = document.getElementsByTagName('body')[0];
         body.className = `${theme}-theme`;
-        setStore('theme', 'theme');
+        setStore('theme', theme);
     }, [searchParams]);
 
     useEffect(() => {
