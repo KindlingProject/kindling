889d9500d32acd2d71991dd103b80aaf2b8750a9
yiqianxu <yiqianxu@harmonycloud.cn>
2022-12-15 19:56:14 +0800

    set traceId
    Signed-off-by: yiqianxu <yiqianxu@harmonycloud.cn>

--- camera-front/src/containers/thread/camera/util.ts
+++ camera-front/src/containers/thread/camera/util.ts
@@ -697,7 +697,7 @@ export const dataHandle = (data: any, timeRange, trace: any) => {
         traceTimes = [minStart, maxEnd];
     }
 
-    return {data: result, eventlist, traceTimes, requestInfo: allInfo, traceId: trace_id};
+    return {data: result, eventlist, traceTimes, requestInfo: allInfo, traceId};
 }
 
 export const getLineTimesList = (requestTimes, traceTimes) => {
