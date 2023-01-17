7fe773eb7cf420c56b0aebe4c21e183a55a328d7
Daxin Wang <daxinwang@harmonycloud.cn>
2023-01-17 11:57:24 +0800

    fix the label name "innerCalls"
    Signed-off-by: Daxin Wang <daxinwang@harmonycloud.cn>

--- collector/pkg/component/analyzer/cpuanalyzer/model.go
+++ collector/pkg/component/analyzer/cpuanalyzer/model.go
@@ -23,7 +23,7 @@ const (
 	JavaFutexEventLabel     = "javaFutexEvents"
 	TransactionIdEventLabel = "transactionIds"
 	SpanLabel               = "spans"
-	InnerCallLabel          = "innerCall"
+	InnerCallLabel          = "innerCalls"
 )
 
 type TimedEvent interface {
