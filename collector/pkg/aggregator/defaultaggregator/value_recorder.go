2ffead22ee1fd72b8a8e4cf702745823827dc158
Daxin Wang <daxinwang@harmonycloud.cn>
2023-02-10 11:15:28 +0800

    Update the CHANGELOG.md
    Signed-off-by: Daxin Wang <daxinwang@harmonycloud.cn>

--- collector/pkg/aggregator/defaultaggregator/value_recorder.go
+++ collector/pkg/aggregator/defaultaggregator/value_recorder.go
@@ -23,8 +23,8 @@ func newValueRecorder(recorderName string, aggKindMap map[string][]KindConfig) *
 }
 
 // Record is thread-safe, and return the result value.
-// A recorder can record only the metrics that are same as the initial ones when using a same key.
-// But it allows to record different metrics with different keys.
+// A recorder can record only the metrics that are the same as the initial ones when using the same key.
+// But it can record different metrics with different keys.
 func (r *valueRecorder) Record(key *aggregator.LabelKeys, metricValues []*model.Metric, timestamp uint64) {
 	if key == nil {
 		return
