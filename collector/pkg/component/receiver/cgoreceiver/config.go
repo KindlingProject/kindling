facab2434e947723f211148ab6e68f5d0f97cbf2
sanyangji <songyujie@zju.edu.cn>
2023-03-30 10:03:13 +0800

    set default config
    Signed-off-by: sanyangji <songyujie@zju.edu.cn>

--- collector/pkg/component/receiver/cgoreceiver/config.go
+++ collector/pkg/component/receiver/cgoreceiver/config.go
@@ -17,6 +17,68 @@ type ProcessFilter struct {
 
 func NewDefaultConfig() *Config {
 	return &Config{
+		SubscribeInfo: []SubEvent{
+			{
+				Name:     "syscall_exit-writev",
+				Category: "net",
+			},
+			{
+				Name:     "syscall_exit-readv",
+				Category: "net",
+			},
+			{
+				Name:     "syscall_exit-write",
+				Category: "net",
+			},
+			{
+				Name:     "syscall_exit-read",
+				Category: "net",
+			},
+			{
+				Name:     "syscall_exit-sendto",
+				Category: "net",
+			},
+			{
+				Name:     "syscall_exit-recvfrom",
+				Category: "net",
+			},
+			{
+				Name:     "syscall_exit-sendmsg",
+				Category: "net",
+			},
+			{
+				Name:     "syscall_exit-recvmsg",
+				Category: "net",
+			},
+			{
+				Name:     "syscall_exit-sendmmsg",
+				Category: "net",
+			},
+			{
+				Name: "kprobe-tcp_close",
+			},
+			{
+				Name: "kprobe-tcp_rcv_established",
+			},
+			{
+				Name: "kprobe-tcp_drop",
+			},
+			{
+				Name: "kprobe-tcp_retransmit_skb",
+			},
+			{
+				Name: "syscall_exit-connect",
+			},
+			{
+				Name: "kretprobe-tcp_connect",
+			},
+			{
+				Name: "kprobe-tcp_set_state",
+			},
+			{
+				Name: "tracepoint-procexit",
+			},
+		},
 		ProcessFilterInfo: ProcessFilter{
 			Comms: []string{"kindling-collec", "containerd", "dockerd", "containerd-shim"},
 		},
