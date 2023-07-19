f440993b9d56c049b428fc4f9b3ce38548eff897
anthonyhui <anthonyhui@126.com>
2023-07-19 11:31:32 +0800

    fix k8sinfoanalyzer config
    Signed-off-by: anthonyhui <anthonyhui@126.com>

--- collector/pkg/component/analyzer/k8sinfoanalyzer/config.go
+++ collector/pkg/component/analyzer/k8sinfoanalyzer/config.go
@@ -1,7 +1,7 @@
 package k8sinfoanalyzer
 
 type Config struct {
-	// send_datagroup_interval is the datagroup sending interval.
+	// SendDataGroupInterval is the datagroup sending interval.
 	// The unit is seconds.
 	SendDataGroupInterval int `mapstructure:"send_datagroup_interval"`
 }
