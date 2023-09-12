a26b9b7b8241a422a9e2f72528da72ffc9fd9e91
YDMsama <ydmsama@gmail.com>
2023-09-12 20:19:53 +0800

    Implemented periodic memory cleanup for OtelExporter
    Signed-off-by: YDMsama <ydmsama@gmail.com>

--- collector/pkg/component/consumer/exporter/otelexporter/prometheus.go
+++ collector/pkg/component/consumer/exporter/otelexporter/prometheus.go
@@ -1,28 +1,44 @@
 package otelexporter
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/Kindling-project/kindling/collector/pkg/component"
 	"go.opentelemetry.io/otel/exporters/prometheus"
 )
 
-func StartServer(exporter *prometheus.Exporter, telemetry *component.TelemetryTools, port string) error {
-	http.HandleFunc("/metrics", exporter.ServeHTTP)
+var (
+	mu   sync.Mutex
+	srv  *http.Server
+)
 
-	srv := http.Server{
+func StartServer(exporter *prometheus.Exporter, telemetry *component.TelemetryTools, port string) error {
+	mu.Lock()
+
+	if srv != nil {
+		if err := srv.Shutdown(context.Background()); err != nil {
+			return fmt.Errorf("failed to stop server: %w", err)
+		}
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/metrics", exporter.ServeHTTP)
+
+	srv = &http.Server{
 		Addr:    port,
-		Handler: http.DefaultServeMux,
+		Handler: mux,
 	}
 
 	telemetry.Logger.Infof("Prometheus Server listening at port: [%s]", port)
-	err := srv.ListenAndServe()
 
-	if err != nil && err != http.ErrServerClosed {
-		return err
-	}
-
-	telemetry.Logger.Infof("Prometheus gracefully shutdown the http server...\n")
+	go func() {
+		mu.Unlock()
+		srv.ListenAndServe()
+		telemetry.Logger.Infof("Prometheus gracefully shutdown the http server...\n")
+	}()
 
 	return nil
 }
